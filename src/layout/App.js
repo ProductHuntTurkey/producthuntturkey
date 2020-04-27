23a53eb8880a8114b27e5b7a2dc6d8c69b923f0e
abdullahsahin <abdullahsahinceo@gmail.com>
2020-04-27 23:40:09 +0300

    App.js içerisinde sayfa yapısı oluşturuldu

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 import './App.css';
 
+import Header from "../components/Header"
+import Developer from "../components/DeveloperInfo"
+import Developer2 from "../components/DeveloperInfo2"
+import OpenSource from "../components/OpenSource"
+import Newsletter from "../components/Newsletter"
+import Products from "../components/Products"
+
+import OpenStartup from "../pages/OpenStartup"
+import AddYourStartup from "../pages/AddYourStartup"
+import NotFound from "../pages/NotFound"
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <OpenSource/>
+        <Header/>
+        <Newsletter />
+        <Developer/>
+        <Developer2/>
+        <Switch>
+          <Route exact path="/" component={Products}/>
+          <Route exact path="/acik-girisim"><OpenStartup/></Route>
+          <Route exact path="/girisimini-ekle"><AddYourStartup/></Route>
+          <Route>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
