69208d5735fb5ef939df715a693605953a25468d
abdullahsahin <abdullahsahinceo@gmail.com>
2020-04-30 23:42:47 +0300

    girisimini-ekle sayfası yapıldı

---
+++ src/pages/girisimini-ekle.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IframeDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Iframe = styled.iframe`
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #EEE; 
+    background:#F5F5F5;
+    width: 65%;
+    height: 500px;
+
+    @media (max-width: 900px) {
+        width: 90%;
+        height: 500px;
+    }
+`;
+
+const AddYourStartupPage = () => (
+    <Layout>
+        <SEO title="Girişimini Ekle"/>
+        <IframeDiv>
+            <Iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSeytVuWJH6XeEKpVc5w9RFOmyqBen6tZD1CyKE4nMk5-PpqXA/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Yükleniyor…</Iframe>
+        </IframeDiv>
+    </Layout>
+)
+
+export default AddYourStartupPage
