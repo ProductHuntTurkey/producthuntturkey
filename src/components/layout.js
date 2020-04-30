17107217f39c7840c4543fe6e1aafdfe93352904
abdullahsahin <abdullahsahinceo@gmail.com>
2020-04-30 23:27:11 +0300

    layout component içerisine gerekli componentler eklendi

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,10 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Developer from "../components/DeveloperInfo"
+import Developer2 from "../components/DeveloperInfo2"
+import OpenSource from "../components/OpenSource"
+import Newsletter from "../components/Newsletter"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,8 +29,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <OpenSource/>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
+        <Newsletter/>
+        <Developer/>
+        <Developer2/>
         <main>{children}</main>
       </div>
     </>
