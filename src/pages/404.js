db74baef8293d8d640f2fcc67664c9244deea3ce
abdullahsahin <abdullahsahinceo@gmail.com>
2020-04-30 23:46:52 +0300

    NotFoundPage sayfasının içeriği güncellendi

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const Container = styled.div`
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    width: 65%;
+`;
+
+const Title = styled.h2`
+    text-align: center;
+    font-weight: bold;
+    font-size: 50px;
+`;
+
+const Content = styled.p`
+    text-align: center;
+    font-size: 25px;
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Title>404</Title>
+      <Content>Uzay'a çıkabiliyoruz, yeni gezegenler keşfedebiliyoruz fakat teknolojimiz o kadar gelişmesine rağmen hala aradığınızı bulamıyoruz.</Content>
+    </Container>
   </Layout>
 )
 
